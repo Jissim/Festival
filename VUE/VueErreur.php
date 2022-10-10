19c63bd8cbee37406ae1f440dbef41c7cfd2908e
Jissim <jissim02@gmail.com>
2022-10-10 15:40:41 +0200

    trie

---
+++ VUE/VueErreur.php
@@ -0,0 +1,12 @@
+<?php $titre = 'Accueil';
+
+ob_start();
+?>
+<p>Une erreur est survenue : <?= $msgErreur ?></p>
+
+<?php $contenu = ob_get_clean();
+
+require 'Vuetemplate.php'; 
+
+echo $contenu;
+?>
