59a9c3bdbc0fba2c10c2cc31a862a716f74ed4de
IkiMeii <berteaux.katia.2003@gmail.com>
2022-12-07 11:34:00 +0100

    Update Template.php

--- controleur/Template.php
+++ controleur/Template.php
@@ -6,7 +6,7 @@ include_once "$racine/modele/bd.inc.php";
 include_once "$racine/modele/Gestion.php";
 
 // appel du script de vue qui permet de gerer l'affichage des donnees
-include "$racine/vue/VueTempla.php";
+include "$racine/vue/VueTemplate.php";
 
 
 ?>
