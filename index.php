6ac916c7343956628cec78eaea3998663ff5db3a
IkiMeii <berteaux.katia.2003@gmail.com>
2022-12-07 10:44:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,12 @@
 <?php
 // require 'Gestion.php';
 
-// try {
-//   $connexion = getConnexion();
-//   require 'Accueil.php';
-// }
-// catch (Exception $e) {
-//  $msgErreur = $e->getMessage();
-//   require 'Erreur.php';
-// }  
 
 include "getRacine.php";
 include "$racine/controleur/controleurPrincipal.php";
 include_once "$racine/modele/bd.inc.php";
-include_once "$racine/modele/Gestion.php"; // pour pouvoir utiliser isLoggedOn()
 
+include_once "$racine/modele/Gestion.php"; 
 // try {
 //   require 'Accueil.php';
 // }
@@ -28,7 +20,20 @@ if (isset($_GET["action"])) {
 } 
 else {
     $action = "template";
+
 }
+catch (Exception $e) {
+ $msgErreur = $e->getMessage();
+  require 'Erreur.php';
+}  
+
+
+//if (isset($_GET["action"])) {
+//    $action = $_GET["action"];
+//} 
+//else {
+//    $action = "accueil";
+//}
 
 $fichier = controleurPrincipal($action);
 include "$racine/controleur/$fichier";
